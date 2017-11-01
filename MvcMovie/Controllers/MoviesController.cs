fde32de6766524a0ed5ad01229d29e26927ba1c9
Francisco Silva <franciscopsilva2@gmail.com>
2017-11-01 20:33:26 +0000

    Modificado o metodo Index de MoviesController, adicionado o modelo MovieGenreViewModel e modificado o ficheiro Index para receber o Modelo MovieGenreViewModel

--- MvcMovie/Controllers/MoviesController.cs
+++ MvcMovie/Controllers/MoviesController.cs
@@ -19,15 +19,28 @@ namespace MvcMovie.Controllers
         }
 
         // GET: Movies
-        public async Task<IActionResult> Index(string id) {
+        public async Task<IActionResult> Index(string movieGenre, string searchString) {
+            // Use LINQ to get list of genres.
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+
             var movies = from m in _context.Movie
                          select m;
 
-            if (!String.IsNullOrEmpty(id)) {
-                movies = movies.Where(s => s.Title.Contains(id));
+            if (!String.IsNullOrEmpty(searchString)) {
+                movies = movies.Where(s => s.Title.Contains(searchString));
             }
 
-            return View(await movies.ToListAsync());
+            if (!String.IsNullOrEmpty(movieGenre)) {
+                movies = movies.Where(x => x.Genre == movieGenre);
+            }
+
+            var movieGenreVM = new MovieGenreViewModel();
+            movieGenreVM.genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+            movieGenreVM.movies = await movies.ToListAsync();
+
+            return View(movieGenreVM);
         }
 
         [HttpPost]
